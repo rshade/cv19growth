8adde2045427fe5ecf6725cc3a5e578b7899872a
Richard Shade <rshade@flexera.com>
2020-03-20 13:55:00 -0500

    adding promise and fetch, learning as I go

--- us.js
+++ us.js
@@ -1,3 +1,17 @@
-var jquery = require("jquery/dist/jquery");
-var data = jquery.getJSON('https://covidtracking.com/api/us')
-console.log(data)
+const fetch = require("node-fetch");
+var url = 'https://covidtracking.com/api/us'
+var census_url='https://api.census.gov/data/2019/pep/natmonthly?get=POP,MONTHLY_DESC&for=us:*'
+var covid_data = function(url){
+  return fetch(url)
+  .then(response => response.json())
+  .then(response => { return response})
+}
+
+console.log(covid_data(url))
+
+fetch(census_url).then(response => response.json())
+    .then(response => {
+            console.log(response);
+        }, error => {
+            console.error(error);
+        });
