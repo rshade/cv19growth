66e8beefbd11113b5c7c923fe295b9f46b6357e2
Richard Shade <rshade@flexera.com>
2020-03-20 11:41:09 -0500

    adding initial work

---
+++ us-based-off-south-korea.js
@@ -0,0 +1,5 @@
+var us_population = 327200000
+var sk_population = 51470000
+// https://fortune.com/2020/03/19/coronavirus-south-korea-test-data/
+var sk_test_count = 295000
+var sk_infection_count = 8652
